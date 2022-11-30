41ab738cf8cf83e52fb6c9f917ed2bd363bd4ca8
Vital24 <zynn00001@gmail.com>
2022-11-30 10:14:33 -0500

    Pop

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,3 +1,18 @@
 import "../styles/style.css";
+import { setupCounter } from "./counter.js";
 
-import { setupCounter } from "./js/counter.js";
+document.querySelector("#app").innerHTML = `
+  <div>
+
+    </a>
+    <h1>Hello Vite!!!!!!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
