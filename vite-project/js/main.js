145b5ef9058f0132fb8708de86e94d72f14ace4d
Vital24 <zynn00001@gmail.com>
2022-12-05 10:16:27 -0500

    AA

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,5 @@
+import "./menu";
 import "../styles/style.css";
-import { setupCounter } from "./counter.js";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
@@ -7,16 +7,6 @@ AOS.init();
 
 document.querySelector("#app").innerHTML = `
   <div>
-
-    </a>
-    <h1>Hello Vite!!!!!!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+      <h1>${beef}</h1>
   </div>
 `;
-
-setupCounter(document.querySelector("#counter"));
---
+++ vite-project/js/menu.js
@@ -0,0 +1,15 @@
+const beef = [
+  "Signature Marbled Beef",
+  "Marbled Steak Cube",
+  "Black Angus Denver Steak",
+];
+const pork = [
+  "BBQ Susuage",
+  "Spicy Pork Chop",
+  "Pork Belly",
+  "Apple Wood Bacon",
+  "Spicy Pork Feet",
+  "Kurobuta Pork Cheeks",
+];
+const All = [beef, pork];
+console.log(All);
